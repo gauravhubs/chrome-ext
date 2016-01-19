f414eddd0593e6ae90150d428a4d7ca418927d9c
gaurav kumar <gauravkr@cse.iitb.ac.in>
2016-01-20 03:38:41 +0530

    Update on

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
-!#/bin/bash
+#!/bin/bash
 
+echo "Update Github chrome-ext"
+touch manifest.json
 git add .
-git commit -a -m "Saved on git "
+git commit -a -m "Update on $date"
 git push http://github.com/gauravhubs/chrome-ext.git master 
