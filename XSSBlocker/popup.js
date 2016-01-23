ca8718fec6bf6eb7f2e67ba3b2328efc4695a97e
gaurav kumar <gauravkr@cse.iitb.ac.in>
2016-01-24 02:48:15 +0530

    Update on Sun Jan 24 02:48:15 IST 2016

--- XSSBlocker/popup.js
+++ XSSBlocker/popup.js
@@ -10,8 +10,8 @@
  */
 
 // onBeforeSendHeaders
-
-
+console.log("Loaded.");
+var flag=0;
 /* extract domain name from given URL. */
 function extractDomainName(url) {
   /* works for http and https URLs only! */
@@ -30,13 +30,19 @@ var callback = function(details) {
       return { cancel:true};
     }    
   }
-  if(method === 'POST'){
-    console.log(details.method);
+  else if(method === 'POST'){
+    //console.log(details);
     // xtract form data and look on posted vata and validate check for script tag
     //var formdata = details.requestBody.formData;
+    //console.log(formdata);
+    //for i in formdata
     //if (details.url.indexOf(script_tag) != -1 ){
     //  return { cancel:true};
     //}    
+    if(flag==1){
+      flag=0;
+      cancel:true;
+    }
   }
   
   else
@@ -45,7 +51,66 @@ var callback = function(details) {
 var filter = {
     urls: ["<all_urls>"]
 };
-var opt_extraInfoSpec = ["blocking"];
+var opt_extraInfoSpec = ["blocking", "requestHeaders"];
 
-chrome.webRequest.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec );
+chrome.webRequest.onBeforeSendHeaders.addListener(callback, filter, opt_extraInfoSpec );
+
+// for post 
+
+
+var callback1 = function(details) {
+  //console.log(details.url);
+  //console.log(details.method);
+  //console.log(details.requestBody.formData);
+  var method = details.method;
+  
+  if(method === 'POST'){
+    //console.log(details.requestBody.formData);
+    // xtract form data and look on posted vata and validate check for script tag
+    //var formdata = details.requestBody.formData;
+    //console.log(formdata);
+    //for i in formdata
+    //if (details.url.indexOf(script_tag) != -1 ){
+    //  return { cancel:true};
+    //} 
+    
+    //for (var i = 0; i < details.requestBody.formData.length; ++i) {
+            //if (details.requestBody.formData[i].indexOf(script_tag) != -1 ) {
+              //console.log(details);
+            //  flag=1; 
+            //  break;
+            //}
+
+            //  console.log(details.requestBody.formData);
+    //}
+
+    var keys = [];
+    var dictionary = details.requestBody.formData;
+    for (var key in dictionary) {
+      if (dictionary.hasOwnProperty(key)) {
+        //keys.push(key);
+        var data1 = dictionary[key];
+        for (var i = 0; i < data1.length; ++i) {
+          if(data1[i].indexOf(script_tag) != -1 ){
+            flag=1;
+          }
+        }
+        console.log(dictionary[key]);
+      }
+    }
+    
+    
+            
+    //console.log(details.requestBody.formData);
+    
+  }
+};
+var filter1 = {
+    urls: ["<all_urls>"]
+};
+var opt_extraInfoSpec1 = ["requestBody"];
+
+
+
+chrome.webRequest.onBeforeRequest.addListener(callback1, filter1, opt_extraInfoSpec1 );
 
